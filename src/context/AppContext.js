172aacdd7734891af92d799e7129d775bbfb5775
unknown <khayatiadel10@gmail.com>
2023-08-07 13:25:21 +0100

    update aa

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client'
 import React from 'react'
 
-const SOCKET_URL = "http://localhost:5001"
+const SOCKET_URL = "https://server-chat-company.onrender.com"
 export const socket =io(SOCKET_URL) 
 //app context
 export const AppContext = React.createContext()
